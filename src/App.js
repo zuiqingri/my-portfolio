d12f1534dfecda9ad3c15762f1740b4dff7c1628
zuiqingri <72846494+zuiqingri@users.noreply.github.com>
2022-01-14 02:40:24 +0800

    entire portfolio

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import SinglePost from "./components/SinglePost";
+import Post from "./components/Post";
+import Project from "./components/Project";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <NavBar />
+      <Routes>
+        <Route element={<Home />} path="/" exact />
+        <Route element={<About />} path="/about" />
+        <Route element={<SinglePost />} path="/post/:slug" />
+        <Route element={<Post />} path="/post" />
+        <Route element={<Project />} path="/project" />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Home.js
@@ -0,0 +1,21 @@
+import React from "react";
+import image from "../monstera-leaf.jpg";
+
+function Home() {
+  return (
+    <main>
+      <img
+        src={image}
+        alt="Monstera Leaves"
+        className="absolute object-cover w-full h-full"
+      />
+      <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
+        <h1 className="text-6xl text-green-100 font-bold cursive leading-none lg:leading-snug home-name">
+          Home Page!
+        </h1>
+      </section>
+    </main>
+  );
+}
+
+export default Home;
